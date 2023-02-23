d40372bf78e7881e06fc9bcf577e1b7d3f7219fb
thangarajdeveloper <thangaraj.developer@gmail.com>
2023-02-23 21:02:09 +0530

    basic app setup

---
+++ backend/app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('blogs/', BlogListView, name='blogs'),
+    path('blog/<int:id>', BlogDetailView, name='blog'),
+    path('addblog/', BlogAdd, name='addblog'),
+    path('updateblog/<int:id>', BlogUpdate, name='updateblog'),
+    path('deleteblog/<int:id>', BlogDelete, name='deleteblog'),
+    path('log/<int:id>', BlogDelete, name='deleteblog')
+]
